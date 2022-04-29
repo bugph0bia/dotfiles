c8600d6b49cec1ec9af08a9a9e17229bc413a5a9
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2022-04-29 13:24:39 +0900

    vimrc に自動コメントアウトの無効化を追加

--- vim/init.sh
+++ vim/init.sh
@@ -3,3 +3,11 @@
 # .vimrc のシンボリックを作成
 # 絶対パスでリンクする必要あり
 ln -s `pwd`/.vimrc ~/.vimrc
+
+# .vimrc の後で読み込まれる設定
+mkdir -p ~/.vim/after/plugin
+cspath=~/.vim/after/plugin/common-setting.vim
+touch $cspath
+# コメント行で行挿入したときの自動コメントアウトを無効化
+echo "au FileType * setlocal formatoptions-=ro" >>$cspath
+
